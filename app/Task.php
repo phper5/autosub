04d11f0bf16a4e3ccb04798b915aa5454bac492d
coder <coder5@qq.com>
2020-06-19 10:17:39 +0800

    支付查询

--- app/Task.php
+++ app/Task.php
@@ -25,74 +25,21 @@ class Task extends Model
             'progress'=>$this->progress,
             'task_id' => $this->id,
             'service' => $this->service,
-            'target_file' => [],
-            'source_file' => [],
         ];
         if ($this->status == self::STATUS_ZM){
             $data['progress'] = 10 + $data['progress']*0.9;
         }
-        $options = [];
-        if (isset($config['preview_width']) && $config['preview_width'])
-        {
-            $options['preview']=1;
-            $options['preview_width'] = $config['preview_width'];
+        if ($data['progress']>100){
+            $data['progress'] = 100;
         }
-        $args = json_decode($this->args,true);
-        if ($this->sub1)
-        {
-
-            if ($this->sub1 && $resource = Resource::find($this->sub1))
-            {
-                $data['sub1'] = $resource->toResponse($options);
-                $data['sub1']['lan_txt'] = Lan::getLanTxt($args['language']);
-            }
-        }
-        if ( $args['is_need_trans'] && $this->sub2)
-        {
-
-            if ($this->sub2 && $resource = Resource::find($this->sub2))
-            {
-                $data['sub2'] = $resource->toResponse($options);
-                $data['sub2']['lan_txt'] = Lan::getLanTxt($args['is_need_trans']);
-            }
-            if ($args['is_need_merge'] && $this->sub3)
-            {
-
-                if ($this->sub3 && $resource = Resource::find($this->sub3))
-                {
-                    $data['sub3'] = $resource->toResponse($options);
-                    $data['sub3']['lan_txt'] = '合并字幕';
-                }
-            }
+        if ($this->args){
+            $args = json_decode($this->args,true);
+        }else{
+            $args=[];
         }
 
-        if ($this->mp3)
-        {
 
-            if ($this->mp3 && $resource = Resource::find($this->mp3))
-            {
-                $data['mp3'] = $resource->toResponse($options);
-            }
-        }
-        if ((isset($config['flac']) && $config['flac'])) {
-            if ($this->flac && $resource = Resource::find($this->flac))
-            {
-                $data['flac'] = $resource->toResponse($options);
-            }
-            if ($this->aac && $resource = Resource::find($this->aac))
-            {
-                $data['aac'] = $resource->toResponse($options);
-            }
-        }
-            if ((isset($config['sourceDetail']) && $config['sourceDetail']) || $this->target_file) {
-            if ($resource = Resource::find($this->source_file))
-            {
-
-                $data['source_file'] = [$resource->toResponse($options)];
-            }
-        }
-
-        $data['args'] = json_decode($this->args);
+        $data['args'] = $args;
 
         return $data;
     }
